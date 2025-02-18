ce11e532c52ad12cd0c163929935a46d7a1d627f
Pierre DAILY <contact@pierredaily.com>
2025-02-18 15:03:58 +0100

    Update Button style

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,7 +4,7 @@ export function Button({ children, className, ...rest }: BtnProps) {
   return (
     <button
       {...rest}
-      className={`${className}  disabled:bg-gray-500 flex justify-center items-center text-white w-16 h-16  hover:bg-red-600  bg-red-500 font-noto -skew-x-12 font-bold`}
+      className={`${className}   disabled:border-gray-400 disabled:text-gray-400 disabled:hover:bg-white flex justify-center items-center text-red-600 border-2 border-red-600 hover:text-white w-16 h-16  hover:bg-red-600  bg-white font-noto -skew-x-12 font-bold`}
     >
       {children}
     </button>
